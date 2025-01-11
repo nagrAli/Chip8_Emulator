c601460183f99543653fd1cef8c9b4317eb3816e
nagrAli <alinagra100@gmail.com>
2025-01-12 00:14:36 +0500

    chore (Cxxx) ! : Adding random number gen in the condition

--- components/instructions/instruction_Cxxx.hpp
+++ components/instructions/instruction_Cxxx.hpp
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 #include <cstdint>
 #include <cpu_components.hpp>
+#include <random>
 
 class instruction_Cxxx
 {
     private:
         static void set_register_to_nn(chip8_cpu_components *components, uint16_t instruction);
+        static uint8_t random_number();
     public:
         static void callback(chip8_cpu_components *components, uint16_t instruction);
 };
