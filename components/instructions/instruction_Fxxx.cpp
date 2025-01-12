ec8c77e85335672fedd5069081343f2684e55716
nagrAli <alinagra100@gmail.com>
2025-01-12 22:42:44 +0500

    feat (instr_0xfxxx) : Callback and wrapper functionality added

--- components/instructions/instruction_Fxxx.cpp
+++ components/instructions/instruction_Fxxx.cpp
@@ -1,9 +1,23 @@
 #include "instruction_Fxxx.hpp"
 
 constexpr uint16_t  VX_MASK = 0x0F00;
+constexpr uint16_t  SUB_INSTRUCTION_MASK = 0x00FF;
 constexpr uint16_t  MAX_KEYS = 16;
 constexpr uint8_t   VX_SHIFT = 8;
 constexpr uint8_t   FONTSET_BYTE_SIZE = 5;
+
+typedef enum
+{
+    FX07 = 0x0007,
+    FX0A = 0x000A,
+    FX15 = 0x0015,
+    FX18 = 0x0018,
+    FX1E = 0x001E,
+    FX29 = 0x0029,
+    FX33 = 0x0033,
+    FX55 = 0x0055,
+    FX65 = 0x0065
+} fxxx_sub_instructions_t;
 
 void instruction_fxxx::fx07(chip8_cpu_components *components, uint16_t instruction)
 {
@@ -74,4 +88,47 @@ void instruction_fxxx::fx65(chip8_cpu_components *components, uint16_t instructi
     {
         components->register_chip8.set_register_value(i, components->memory_chip8.read_instruction(components->index_register_chip8 + i));
     }
+}
+
+void instruction_fxxx::execute_instruction(chip8_cpu_components *components, uint16_t instruction)
+{
+    fxxx_sub_instructions_t sub_instruction = static_cast<fxxx_sub_instructions_t>(instruction & SUB_INSTRUCTION_MASK);
+    switch(sub_instruction)
+    {
+        case FX07:
+            fx07(components, instruction);
+            break;
+        case FX0A:
+            fx0A(components, instruction);
+            break;
+        case FX15:
+            fx15(components, instruction);
+            break;
+        case FX18:
+            fx18(components, instruction);
+            break;
+        case FX1E:
+            fx1E(components, instruction);
+            break;
+        case FX29:
+            fx29(components, instruction);
+            break;
+        case FX33:
+            fx33(components, instruction);
+            break;
+        case FX55:
+            fx55(components, instruction);
+            break;
+        case FX65:
+            fx65(components, instruction);
+            break;
+        default:
+            std::cerr << "Unknown instruction: " << instruction << std::endl;
+            break;
+    }
+}
+
+void instruction_fxxx::callback(chip8_cpu_components *components, uint16_t instruction)
+{
+    execute_instruction(components, instruction);
 }
