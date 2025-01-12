1075655afaacfd0b5cdab7c1824017e9e6cc0241
nagrAli <alinagra100@gmail.com>
2025-01-12 21:43:15 +0500

    feat(instr_fx0A) : Addition of instruction 0xFX0A

--- components/instructions/instruction_Fxxx.cpp
+++ components/instructions/instruction_Fxxx.cpp
@@ -2,9 +2,24 @@
 
 constexpr uint16_t VX_MASK = 0x0F00;
 constexpr uint8_t VX_SHIFT = 8;
+constexpr uint16_t MAX_KEYS = 16;
 
 void instruction_fxxx::fx07(chip8_cpu_components *components, uint16_t instruction)
 {
     uint8_t x = (instruction & VX_MASK) >> VX_SHIFT;
     components->register_chip8.set_register_value(x, components->timer_chip8);
+}
+
+void instruction_fxxx::fx0A(chip8_cpu_components *components, uint16_t instruction)
+{
+    uint8_t x = (instruction & VX_MASK) >> VX_SHIFT;
+    for(uint8_t i = 0; i < MAX_KEYS; i++)
+    {
+        if(components->keypad_chip8.get_key(i))
+        {
+            components->register_chip8.set_register_value(x, i);
+            return;
+        }
+    }
+    components->program_counter_chip8 -= 2;
 }
